19b48b897374c94a257a7377ae16b4eaba669f97
Nikita Volchenko <volcenko.nikitka@mail.ru>
2021-10-14 16:38:59 +0300

    ninth commit

--- src/Act2.java
+++ src/Act2.java
@@ -1,7 +1,6 @@
-public class Act2 {
-    public int startAct(Character character) {
-        System.out.println("========== Акт второй ==========");
-
+public class Act2 extends Act{
+    @Override
+    public Character makeMonster() {
         Four[] clothes = new Four[4];
         clothes[0] = new Four("helmet", 0, 5, true);
         clothes[1] = new Four("shoes", 0, 5, false);
@@ -14,13 +13,6 @@ public class Act2 {
         skills[2] = new Four("protection", 0, 2, true);
         skills[3] = new Four("healing", 0, 0, true);
 
-        Character monster = new Character("Monster2", clothes, skills, 70, 0);
-
-        Battle battle = new Battle();
-        battle.startBattle(character, monster);
-
-        Check check = new Check();
-
-        return check.checkWinner(character, 2);
+        return new Character("Monster2", clothes, skills, 70, 0);
     }
 }
